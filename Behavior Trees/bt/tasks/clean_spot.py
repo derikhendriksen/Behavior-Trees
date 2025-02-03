2d3c22006ce7bf61d452170d6efbf275905b602c
derikhendriksen <150965565+derikhendriksen@users.noreply.github.com>
2025-02-03 08:42:20 -0500

    Delete Behavior Trees directory

--- Behavior Trees/bt/tasks/clean_spot.py
+++
@@ -1,9 +0,0 @@
-import bt_library as btl
-
-
-class CleanSpot(btl.Task):
-    def run(self, blackboard: btl.Blackboard) -> btl.ResultEnum:
-        self.print_message("Cleaning Spot")
-
-        return self.report_running(blackboard)
-
--- Behavior Trees/bt/tasks/do_nothing.py
+++
@@ -1,7 +0,0 @@
-import bt_library as btl
-
-
-class DoNothing(btl.Task):
-    def run(self, blackboard: btl.Blackboard) -> btl.ResultEnum:
-        self.print_message("Doing Nothing")
-        return self.report_succeeded(blackboard)
